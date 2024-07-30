36e81b8542bf887d41bcedc2de7064fc8988880d
intaniger <intaniger@gmail.com>
2024-07-30 11:03:27 +0700

    fix(create): :bug: add validation before submitting - also add alert component to display errors other than validation.

--- src/components/common/InputGroup.svelte
+++ src/components/common/InputGroup.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   export let id: string;
   export let title: string;
+  export let fieldError: string[] | undefined;
+
+  $: errorClass =
+    fieldError && fieldError.length > 0 ? "border border-app-danger p-2" : "";
 </script>
 
-<div class="flex flex-col gap-y-2">
+<div class="flex flex-col gap-y-2 {errorClass}">
+  {#if fieldError && fieldError.length > 0}
+    <div class="flex flex-col">
+      {#each fieldError as error}
+        <p class="text-sm font-default text-app-danger">{error}</p>
+      {/each}
+    </div>
+  {/if}
   <label class="text-base font-semibold text-app-black" for={id}>{title}</label>
   <slot />
 </div>
--- src/components/page/data_mapping/DataOperationsBar/CreateDataForm.svelte
+++ src/components/page/data_mapping/DataOperationsBar/CreateDataForm.svelte
@@ -2,6 +2,7 @@
   import Button from "@common/Button.svelte";
   import InputGroup from "@common/InputGroup.svelte";
   import MultiSelect from "@common/MultiSelect.svelte";
+  import { AxiosError } from "axios";
   import { CONTEXT_KEY_DATA_MAPPING_REPOSITORY } from "constants/contextKeys";
   import EVENTS from "constants/events";
   import {
@@ -10,6 +11,22 @@
     type IDataMappingRepository,
   } from "repository/types";
   import { createEventDispatcher, getContext } from "svelte";
+  import { z, ZodError } from "zod";
+
+  const DataMappingScheme = z.object({
+    title: z.string().min(1),
+    description: z.string().optional(),
+    department: z.enum(["HR", "IT", "ADMISSION", "MARKETING"], {
+      message:
+        "Please select one of Human Resources, IT/IS, Admission, Marketing as a department",
+    }),
+    subjectTypes: z
+      .array(z.enum(["EMPLOYEE", "FACULTY_STAFF", "STUDENT"]))
+      .min(0),
+  });
+
+  type Form = z.infer<typeof DataMappingScheme>;
+  type FieldErrors = { [k in keyof Form]?: string[] };
 
   const dispatch = createEventDispatcher();
   const repo = getContext<IDataMappingRepository>(
@@ -17,19 +34,20 @@
   );
 
   // Form Data
-  let title: string;
-  let description: string;
-  let department: keyof typeof DataMappingDepartmentTranslation;
-  let subjectTypes: Set<keyof typeof DataMappingDataSubjectTypeTranslation>;
+  let title: Form["title"];
+  let description: Form["description"];
+  let department: Form["department"];
+  let subjectTypes: Set<Form["subjectTypes"][number]>;
+  let validationErrors: FieldErrors = {};
+  let otherError: unknown;
 
   const resetForm = () => {
-    title = undefined as unknown as string;
-    description = undefined as unknown as string;
-    department =
-      undefined as unknown as keyof typeof DataMappingDepartmentTranslation;
-    subjectTypes = new Set<
-      keyof typeof DataMappingDataSubjectTypeTranslation
-    >();
+    title = undefined as unknown as Form["title"];
+    description = undefined as unknown as Form["description"];
+    department = undefined as unknown as Form["department"];
+    subjectTypes = new Set<Form["subjectTypes"][number]>();
+    validationErrors = {};
+    otherError = undefined;
   };
 
   const onClose = () => {
@@ -38,14 +56,30 @@
   };
 
   const onCreate = async () => {
-    await repo.create({
-      title,
-      description,
-      department,
-      data_subject_type: [...subjectTypes],
-    });
-    resetForm();
-    dispatch(EVENTS.CLOSE_FORM);
+    try {
+      DataMappingScheme.parse({
+        title,
+        description,
+        department,
+        subjectTypes: [...subjectTypes],
+      });
+      await repo.create({
+        title,
+        description,
+        department,
+        data_subject_type: [...subjectTypes],
+      });
+      resetForm();
+      dispatch(EVENTS.CLOSE_FORM);
+    } catch (error: unknown) {
+      if (error instanceof ZodError) {
+        validationErrors = error.formErrors.fieldErrors;
+      } else if (error instanceof AxiosError) {
+        otherError = error.message;
+      } else {
+        otherError = error;
+      }
+    }
   };
 </script>
 
@@ -59,7 +93,16 @@
   </div>
 </div>
 <div class="py-4 px-6 flex flex-col gap-y-6">
-  <InputGroup title="Title" id="new-title">
+  {#if otherError}
+    <div role="alert" class="alert alert-error">
+      Failed to create data: {otherError}
+    </div>
+  {/if}
+  <InputGroup
+    title="Title"
+    id="new-title"
+    fieldError={validationErrors["title"]}
+  >
     <input
       id="new-title"
       type="text"
@@ -68,7 +111,11 @@
     />
   </InputGroup>
 
-  <InputGroup title="Description" id="new-description">
+  <InputGroup
+    title="Description"
+    id="new-description"
+    fieldError={validationErrors["description"]}
+  >
     <textarea
       id="new-description"
       class="border border-app-input-border rounded-md shadow-textbox py-2 px-3 h-19 resize-none"
@@ -76,7 +123,11 @@
     />
   </InputGroup>
 
-  <InputGroup title="Department" id="new-department">
+  <InputGroup
+    title="Department"
+    id="new-department"
+    fieldError={validationErrors["department"]}
+  >
     <select
       id="new-department"
       class="select py-2 px-3 border border-app-input-border rounded-md min-h-10 h-10 text-sm font-normal font-default"
@@ -89,7 +140,11 @@
     </select>
   </InputGroup>
 
-  <InputGroup title="Data Subject Type (Optional)" id="new-data-subject-type">
+  <InputGroup
+    title="Data Subject Type (Optional)"
+    id="new-data-subject-type"
+    fieldError={validationErrors["subjectTypes"]}
+  >
     <MultiSelect
       id="new-data-subject-type"
       options={Object.entries(DataMappingDataSubjectTypeTranslation)}
