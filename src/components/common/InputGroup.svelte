2dd65f0fc6882b47498fc5614d20381149689460
intaniger <intaniger@gmail.com>
2024-07-31 16:59:18 +0700

    style(components): :children_crossing: improve InputGroup error state styling

--- src/components/common/InputGroup.svelte
+++ src/components/common/InputGroup.svelte
@@ -2,19 +2,21 @@
   export let id: string;
   export let title: string;
   export let fieldError: string[] | undefined;
-
-  $: errorClass =
-    fieldError && fieldError.length > 0 ? "border border-app-danger p-2" : "";
+  import "styles/input_group.css";
+  $: titleColor =
+    fieldError && fieldError.length > 0 ? "text-app-danger" : "text-app-black";
 </script>
 
-<div class="flex flex-col gap-y-2 {errorClass}">
+<div class="flex flex-col gap-y-2">
+  <label class="text-base font-semibold {titleColor}" for={id}>{title}</label>
   {#if fieldError && fieldError.length > 0}
-    <div class="flex flex-col">
+    <div class="flex flex-col gap-y-2 error">
       {#each fieldError as error}
-        <p class="text-sm font-default text-app-danger">{error}</p>
+        <p class="text-xs font-default text-app-danger">{error}</p>
       {/each}
+      <slot />
     </div>
+  {:else}
+    <slot />
   {/if}
-  <label class="text-base font-semibold text-app-black" for={id}>{title}</label>
-  <slot />
 </div>
